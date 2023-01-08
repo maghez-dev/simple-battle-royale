def6a74d529354fa6bab2f702d2ca78c40f74ecb
Maghez <ghezzi96yes@gmail.com>
2023-01-08 21:12:19 +0100

    Generate enemies

---
+++ Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public int enemiesCreated;
+
+    void Start()
+    {
+        StartCoroutine(CreateEnemies());
+    }
+
+    IEnumerator CreateEnemies()
+    {
+        int numberOfEnemies = 10;
+        var yPosition = 0.5f;
+
+        while (enemiesCreated < numberOfEnemies)
+        {
+            var xPosition = Random.Range(-25, 25);
+            var zPosition = Random.Range(-25, 25);
+            
+            Instantiate(enemyPrefab, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity);
+            
+            enemiesCreated++;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
